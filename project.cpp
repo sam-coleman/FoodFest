1ea4103638a00a545ad24f55ffdf3fafd2c5cf6e
kat <k82019@gmail.com>
2021-04-11 21:37:46 -0400

    Add initial files

---
+++ project.cpp
@@ -0,0 +1,23 @@
+/**
+ * Food Fest Game
+ * 
+ * @author Sam Coleman
+ * @author Kate Mackowiak
+ */
+
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <stdlib.h>
+#include <tuple>
+#include <iostream>
+#include <unistd.h>
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv ) {
+
+    return 0;
+}
