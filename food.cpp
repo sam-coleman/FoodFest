5580c54587c7a66d98bc89dbbf8a36dc77bd6ccf
kat <k82019@gmail.com>
2021-04-26 22:18:42 -0400

    Shrink food

--- food.cpp
+++ food.cpp
@@ -9,7 +9,7 @@ Food::Food(){
     string imgAddress = imgAddresses[rand() % imgAddresses.size()];
     //cout<<imgAddress<<endl;
     img = imread(imgAddress, -1);
-    cv::resize(img, img, Size(40,40)); 
+    // cv::resize(img, img, Size(40,40)); 
     //srand(time(0));
     //coordinates = Rect(rand()%(620-20 + 1)+20,rand()%(460-50 + 1)+50,img.size().width,img.size().height);
     coordinates = Rect(rand()%(620-img.size().width-20 + 1)+20,rand()%(460-img.size().height-50 + 1)+50,img.size().width,img.size().height);
