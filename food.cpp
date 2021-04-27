2cc42cc9ce534ccd898398b256c333bb0562b5ec
Sam Coleman <scoleman@olin.edu>
2021-04-26 22:53:02 -0400

    Falling food

--- food.cpp
+++ food.cpp
@@ -12,7 +12,7 @@ Food::Food(){
     // cv::resize(img, img, Size(40,40)); 
     //srand(time(0));
     //coordinates = Rect(rand()%(620-20 + 1)+20,rand()%(460-50 + 1)+50,img.size().width,img.size().height);
-    coordinates = Rect(rand()%(620-img.size().width-20 + 1)+20,rand()%(460-img.size().height-50 + 1)+50,img.size().width,img.size().height);
+    coordinates = Rect(rand()%(620-img.size().width-20 + 1)+20,1,img.size().width,img.size().height);
 
     //cout << "x cord: " << coordinates.x << "y cord: " << coordinates.y << endl;
 
